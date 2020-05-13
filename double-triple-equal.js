5c5993b9bbbce567b64a8b827446cbe577099018
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-05-13 23:10:26 +0600

    Double Equal VS Triple Equal

---
+++ double-triple-equal.js
@@ -0,0 +1,37 @@
+/**
+ * Double Equal VS Triple Equal
+ */
+
+const first  = 2;
+const second = "2";
+if(first == second) {
+    console.log("Condition1 is true");
+} else {
+    console.log("Condition1 is false");
+}
+
+
+const first2  = 2;
+const second2 = "2";
+if(first2 === second2) {
+    console.log("Condition2 is true");
+} else {
+    console.log("Condition2 is false");
+}
+
+
+const first3  = 1;
+const second3 = true;
+if(first3 == second3) {
+    console.log("Condition3 is true");
+} else {
+    console.log("Condition3 is false");
+}
+
+const first4  = 1;
+const second4 = true;
+if(first4 === second4) {
+    console.log("Condition4 is true");
+} else {    
+    console.log("Condition4 is false");
+}
