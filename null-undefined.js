4e7eaf3d5df4fbbb49bd15e9fd47f1ca8a0e1df5
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-05-13 22:53:52 +0600

    Null VS Undefined

---
+++ null-undefined.js
@@ -0,0 +1,52 @@
+
+/**
+ * undefined
+ */
+let pakhi;
+console.log(pakhi);
+
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+const result = add(12, 54);
+console.log(result);
+
+function add2(num1, num2) {
+    console.log(num1 + num2);
+    return;
+}
+
+const result2 = add2(12, 58);
+console.log(result2);
+
+function add3(num1, num2) {
+    console.log(num2);
+    return;
+}
+
+const result3 = add3(13);
+console.log(result3);
+
+
+const premik = {name:"Smart dude", phone:458422};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
+
+/**
+ * null
+ */
+
+
+ let basket = null;
+ console.log(basket);
+
+
+
+
